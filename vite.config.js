48b83aea33fbf7f7b607b9481fb2f729ddad9d58
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2025-06-29 14:43:38 +0300

    Fix icon link for footer

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: '/project-TeamFrog/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
